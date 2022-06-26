57f2c1c6ababf302f238257f06d3210aa1e96218
prairiepavement <84539524+prairiepavement@users.noreply.github.com>
2022-06-26 13:32:08 -0500

    set parser to get user input from cli

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 # weather.py
 
 from configparser import ConfigParser
+import argparse
 
 
 def _get_api_key():
@@ -15,3 +16,33 @@ def _get_api_key():
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
+
+
+def read_user_cli_args():
+    """
+    Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    
+    parser.add_argument(
+        "city", nargs="+", type=str, help="enter the city name"
+    )
+
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
+
+
+
+
+if __name__ == "__main__":
+    read_user_cli_args()
