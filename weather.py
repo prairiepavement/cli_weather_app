e92a0bff68d04a0d7e00215bc822f792ca861cdd
prairiepavement <84539524+prairiepavement@users.noreply.github.com>
2022-06-27 12:54:53 -0500

    construct build_weather_query function

--- weather.py
+++ weather.py
@@ -2,6 +2,10 @@
 
 from configparser import ConfigParser
 import argparse
+from urllib import parse
+
+
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 
 def _get_api_key():
@@ -42,6 +46,26 @@ def read_user_cli_args():
     return parser.parse_args()
 
 
+def build_weather_query(city_input, imperial=False):
+    """
+    Builds the URL for an API request to OpenWeather's API
+
+    Args:
+        city_input (List[str]): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+    
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name endpoint
+    """
+    api_key = _get_api_key()
+    city_name = " ".join(city_input)
+    url_encoded_city_name = parse.quote_plus(city_name)
+    units = "imperial" if imperial else "metric"
+    url = (
+        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+        f"&units={units}&appid={api_key}"
+    )
+    return url
 
 
 if __name__ == "__main__":
